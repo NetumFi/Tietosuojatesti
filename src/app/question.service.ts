69d2333889cd0a3e7ccaeddeacaa34a49e62bb46
Heikki Saarineva <heikki.saarineva@kolumbus.fi>
2017-04-19 13:31:43 +0300

    Max points and user points are calculated when user moves to the next page

--- src/app/question.service.ts
+++ src/app/question.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Question, Option, Answer, Points } from './questions/questions.model';
+import { Question, Option, Answer } from './questions/questions.model';
 import { User } from './user/user.model';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
@@ -13,15 +13,22 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 @Injectable()
 export class QuestionService {
 
-  private answers: Answer[] = [];
-
-  private allPoints: Observable<Points[]>;
+  private userPoints = 0;
+  private maxPoints = 0;
 
   private user: User = { name: '', title: '', organization: '' };
 
   private pageNumber: BehaviorSubject<number> = new BehaviorSubject(1);
 
   constructor(public http: Http) { }
+
+  setUserPoints(userPoints) {
+    this.userPoints = userPoints;
+  }
+
+  setMaxPoints(maxPoints) {
+    this.maxPoints = maxPoints;
+  }
 
   getQuestions(): Observable<Question[]> {
     return this.http.get('assets/questions.json')
@@ -32,43 +39,8 @@ export class QuestionService {
     return this.pageNumber;
   }
 
-  setPageNumber(pageNumber) {
+  setPageNumber(pageNumber: number) {
     this.pageNumber.next(pageNumber);
-  }
-
-
-  getGivenAnswers(question: Question): Answer[] {
-    const answers: Answer[] = [];
-    question.choices.forEach((option: Option) => {
-      this.answers.filter((answer: Answer) => {
-        return option.id === answer.optionId;
-      }).forEach((answer: Answer) => {
-        answers.push(answer);
-      });
-    });
-    if (answers.length === 0) {
-      question.choices.forEach((option: Option) => {
-        answers.push({'optionId': option.id, 'checked': false});
-      });
-    }
-    return answers;
-  }
-
-  updateAnswer(answers: Answer[]) {
-    answers.forEach(answer => {
-      const matched: Answer[] = this.answers.filter(oldAnswer => {
-        return answer.optionId === oldAnswer.optionId;
-      });
-      if (matched.length === 0) {
-        this.answers.push(answer);
-      } else {
-        this.answers.forEach((oldAnswer, index) => {
-          if (answer.optionId === oldAnswer.optionId) {
-            this.answers[index] = answer;
-          }
-        });
-      }
-    });
   }
 
   getUser() {
@@ -79,56 +51,12 @@ export class QuestionService {
     this.user = user;
   }
 
-  calculatePoints(): Observable<number> {
-    if (this.allPoints == null) {
-      this.loadPoints();
-    }
-    return this.allPoints.map(allPoints => {
-      let sum = 0;
-      this.answers.forEach((answer: Answer) => {
-        allPoints.filter((points: Points) => {
-          return points.optionId === answer.optionId && answer.checked;
-        }).forEach((points: Points) => {
-          sum += points.pointsIfChecked;
-        });
-      });
-      return sum;
-    });
+  getUserPoints() {
+    return this.userPoints;
   }
 
-  calculateMaxPoints(): Observable<number> {
-    if (this.allPoints == null) {
-      this.loadPoints();
-    }
-    const optionIds: string[] = [];
-    this.getQuestions().subscribe(questions => {
-      questions.forEach(question => {
-        question.choices.forEach(option => optionIds.push(option.id));
-      });
-    });
-    return this.allPoints.map(allPoints => {
-      let sum = 0;
-      allPoints.filter((points: Points) => {
-        return optionIds.some(optionId => points.optionId === optionId && points.pointsIfChecked > 0);
-      }).forEach((points: Points) => {
-        sum += points.pointsIfChecked;
-      });
-      return sum;
-    });
+  getMaxPoints() {
+    return this.maxPoints;
   }
 
-  calculatePercentage(): Observable<number> {
-    return Observable.forkJoin(this.calculatePoints(), this.calculateMaxPoints()).map(data => {
-      let percentage = 0;
-      if (data[1] > 0) {
-        percentage = data[0] * 100 / data[1];
-      }
-      return percentage;
-    });
-  }
-
-  private loadPoints() {
-    this.allPoints = this.http.get('assets/points.json')
-      .map(response => response.json());
-  }
 }
