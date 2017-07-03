ae78ee1e76e8428f09c8abc27ea83aa986134cbf
Heikki Saarineva <heikki.saarineva@netum.fi>
2017-07-03 15:14:30 +0300

    Cleaned up code

--- src/app/backing-up-from-results-not-allowed-guard.ts
+++ src/app/backing-up-from-results-not-allowed-guard.ts
@@ -3,15 +3,14 @@ import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angul
 @Injectable()
 export class BackingUpFromResultsNotAllowedGuard implements CanActivate {
 
-  lastPageResults = false;
+  previousPageResults = false;
 
   constructor() { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    console.log('Route: ' + route.url.toString());
-    const access = !this.lastPageResults || route.url.toString().indexOf('kysymykset') === -1;
+    const access = !this.previousPageResults || route.url.toString().indexOf('kysymykset') === -1;
     if (access) {
-      this.lastPageResults = state.url.toString().indexOf('tulokset') !== -1;
+      this.previousPageResults = state.url.toString().indexOf('tulokset') !== -1;
     }
     return access;
   }
